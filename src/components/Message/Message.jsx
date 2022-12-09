03f5d35e296ec10cf9d1bc5df3dc6f7c9bfc32f4
TUGULDUR URANTOGOS <persiaze@gmail.com>
2022-12-10 01:45:03 +0800

    chats done

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,7 +1,14 @@
+import { Avatar } from "antd";
 import React from "react";
 
-const Message = ({ userData }) => {
-  return <div>Message</div>;
+const Message = ({ signedUser ,selectedFriend, message }) => {
+  return (
+    console.log("msg", message), 
+    <div className={selectedFriend.userId === message.sentBy ? "flex max-h-[calc(100vh-60px)] py-1 gap-1 scroll-smooth" : "flex flex-row-reverse max-h-[calc(100vh-60px)]   py-1 gap-1 scroll-smooth"}>
+      <Avatar src={selectedFriend.userId === message.sentBy ? selectedFriend.userImg : signedUser.userImg} />
+      <div className={selectedFriend.userId === message.sentBy ? "bg-purple-100 p-3 min-w-[50vw] rounded-r-lg rounded-bl-lg" : "bg-slate-100 p-3 min-w-[50vw] rounded-b-lg rounded-tl-lg"}>{message.msgBody}</div>
+    </div>
+  )
 };
 
 export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,6 +13,7 @@ import {
   arrayUnion,
 } from "firebase/firestore";
 import { db } from "../../firebase.config";
+import Message from "../../components/Message/Message";
 
 export const Home = () => {
   const [menuShown, setMenuShown] = useState(false);
@@ -21,7 +22,9 @@ export const Home = () => {
   const [friends, setFriends] = useState([]);
   const [selectedFriend, setSelectedFriend] = useState([]);
   const [docData, setDocData] = useState([]);
-  const [conversationId, setConversationId] = useState("");
+  const [chatId, setChatId] = useState("");
+  const [messages, setMessages] = useState([])
+  const [joke,setJoke]= useState([]);
 
   const fetchSignedUserDoc = async () => {
     const docRef = doc(db, "users", signedUser.uid);
@@ -39,42 +42,63 @@ export const Home = () => {
     });
   };
 
-  const handleSend = async (e) => {
-    const newId =
-      signedUser.uid > selectedFriend.userId
-        ? signedUser.uid + selectedFriend.userId
-        : selectedFriend.userId + signedUser.uid;
-
-    setConversationId(newId);
-
-    const docRef = doc(db, "chats", newId);
-    const unsub = onSnapshot(doc(db, "chats", newId), (doc) => {
-      setDocData(doc.data());
-      console.log("messages", doc.data());
-    });
-
-    if (docData.exists) {
-      await updateDoc(docRef, {
-        messages: arrayUnion({
-          date: new Date(),
-          msgBody: e.target.value,
-          sentBy: signedUser.uid,
-        }),
-      }).then(() => console.log("doc updated"));
-    } else {
-      await setDoc(docRef, {
-        messages: {
-          date: new Date(),
-          msgBody: e.target.value,
-          sentBy: signedUser.uid,
-        },
-      }).then(() => console.log("new doc created"));
-    }
+  const generateChatId = (friend) => {
+    return signedUser.uid > friend.userId
+      ? signedUser.uid + friend.userId
+      : friend.userId + signedUser.uid;
   };
 
-  const handleSelect = (friend) => {
-    setSelectedFriend(friend);
-    console.log(selectedFriend);
+  const handleSend = async (e) => {
+    const newId = generateChatId(selectedFriend);
+    const docRef = doc(db, "chats", newId);
+
+    const unsub = onSnapshot(doc(db, "chats", newId), (doc) => {
+      messages.push(doc.data());
+      console.log("tugudlur",messages)
+    });
+
+    await updateDoc(docRef, {
+      messages: arrayUnion({
+        date: new Date(),
+        msgBody: e.target.value,
+        sentBy: signedUser.uid,
+      }),
+    }).then(() => {
+      const unsub = onSnapshot(docRef, (doc) => {
+        messages.push(doc.data());
+      });
+      setMessages(messages)
+      
+      console.log("doc updated", messages);
+    });
+
+    e.target.value = "";
+  };
+
+
+   const handleSelect = async (friend) => {
+    const newId = generateChatId(friend);
+    setChatId(newId);
+    const docRef = doc(db, "chats", newId);
+    const docSnap = await getDoc(docRef);
+
+    setSelectedFriend(friend); // useless now but may use later
+
+    if (!docSnap?.exists()) {
+      await setDoc(docRef, { messages: []}).then(() =>
+        console.log("new chat added")
+      );
+    } else {
+      fetchMessages(newId);
+    }
+  };
+    const fetchMessages =  (chatId) => {
+    const unsub =  onSnapshot(doc(db, "chats", chatId), (doc) => {
+      messages.push(doc?.data());
+      setMessages(messages);
+      console.log("fetched2", messages);
+      setJoke(messages)
+    });
   };
 
   useEffect(() => {
@@ -84,8 +108,8 @@ export const Home = () => {
   return (
     <>
       {menuShown && <Drawer />}
-      <div className="h-screen flex items-center justify-center">
-        <div className="flex flex-col items-center w-[50px]   h-full">
+      <div className="h-[100vh] flex items-center justify-center">
+        <div className="flex flex-col items-center w-[50px] h-full">
           <div className="menuIcon">
             <Avatar
               className="flex justify-center items-center "
@@ -95,7 +119,7 @@ export const Home = () => {
             />
           </div>
           <div className="flex flex-col overflow-y-scroll scrollbar-hide h-[calc(100%-50px)] my-2">
-            {friends.length > 0 &&
+            {friends?.length > 0 &&
               friends?.map((friend, index) => (
                 <div className="mb-1" key={index}>
                   <Avatar
@@ -110,29 +134,32 @@ export const Home = () => {
             <Avatar
               className="flex justify-center items-center"
               size={40}
-              src={signedUserData.userImg}
+              src={signedUserData?.userImg}
             />
           </div>
         </div>
         <div className="w-full h-full">
-          <div className="flex items-center justify-center h-100 border border-blue-800 h-[5vh] py-[15px]">
+          <div className="flex items-center justify-center border-b h-100 h-[40px] py-[15px]">
             <p>{selectedFriend.displayName}</p>
           </div>
-          <div className="flex flex-col border border-red-800 h-[95vh]">
-            <div className="chats flex-1">
-              <p>chats</p>
-            </div>
-
-            <div className="flex justify-end gap-[2vw] mx-[2vw] my-[5px]">
-              <Avatar />
-              <Avatar />
-              <Input
-                className="justify-end w-[50vw]"
-                placeholder="msg bichne uu"
-                onPressEnter={(e) => handleSend(e)}
-              />
+          <div className="flex flex-col flex-1 h-[calc(100vh-80px)] overflow-y-auto">
+            <div className="chats flex-1 p-3">
+                {joke && selectedFriend && messages[messages.length-1]?.messages.map((msg,index) => (
+                  <Message key={index} signedUser={signedUserData} selectedFriend={selectedFriend} message={msg}/>
+                ))}
             </div>
           </div>
+          <div className="flex justify-end items-end h-[40px] pb-1" >
+              <div className="flex justify-end gap-[2vw] mx-[2vw]">
+                <Avatar />
+                <Avatar />
+                <Input
+                  className="justify-end w-[50vw]"
+                  placeholder="msg bichne uu"
+                  onPressEnter={(e) => handleSend(e)}
+                />
+              </div>
+            </div>
         </div>
       </div>
     </>
